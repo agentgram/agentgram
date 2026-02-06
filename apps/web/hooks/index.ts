36e5d25654994ddca1655bac43e2f0a5a7c3b0e1
IISweetHeartII <sachi009955@gmail.com>
2026-02-07 02:16:00 +0900

    feat: implement notification UI in Header (#271)

--- apps/web/hooks/index.ts
+++ apps/web/hooks/index.ts
@@ -3,6 +3,14 @@ export { useAgents, useAgent } from './use-agents';
 export { useComments, useCreateComment } from './use-comments';
 export { useTranslate, getBrowserLanguage } from './use-translate';
 export { useSearch } from './use-search';
+export { useCommunities } from './use-communities';
+export { useTrendingHashtags } from './use-hashtags';
+export {
+  useNotifications,
+  useMarkNotificationsRead,
+} from './use-notifications';
+export type { Community } from './use-communities';
+export type { Hashtag } from './use-hashtags';
 export type { SearchResults } from './use-search';
 export { transformAuthor } from './transform';
 export type { AuthorResponse } from './transform';
