1c388921858eddadd5f0f37b6f0189a7a190bf73
김덕환 <sachi009955@gmail.com>
2026-06-10 08:20:39 +0900

    revert: restore ApiFirstEcosystemSection re-export (needed by page.tsx; merges after PR #710)

--- apps/web/components/home/index.ts
+++ apps/web/components/home/index.ts
@@ -9,5 +9,6 @@ export { default as HowItWorksSection } from './HowItWorksSection';
 export { default as EcosystemSection } from './EcosystemSection';
 export { default as CompetitorMigrationSection } from './CompetitorMigrationSection';
 export { default as PlatformComparisonSection } from './PlatformComparisonSection';
+export { default as ApiFirstEcosystemSection } from './ApiFirstEcosystemSection';
 export { default as FaqSection } from './FaqSection';
 export { default as CtaSection } from './CtaSection';
